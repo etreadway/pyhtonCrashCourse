78305a00cad420cb4d60558fecd06f66079c43d1
lowtierbeer <eric.s.treadway@gmail.com>
2020-09-21 14:25:20 -0500

    made the code more concise using the items method

--- favoriteLanguage.py
+++ favoriteLanguage.py
@@ -12,7 +12,31 @@ favLanguage = {
 #       favLanguage['sarah'].title() +
 #       '.')
 
-# printing using a for loop
-for name, language in favLanguage.items():
-    print(name.title() + '\'s favorite language is '
-          + language.title() + '.')
+# # printing using a for loop
+# for name, language in favLanguage.items():
+#     print(name.title() + '\'s favorite language is '
+#           + language.title() + '.')
+#
+#
+# print('-' * 20)
+# print('This is how I was doing it')
+# print('\n')
+# for name in favLanguage:
+#     print(name.title())
+#
+# # this does the same thing but is more explicit
+# print('-' * 20)
+# print('This is the .keys() method')
+# print('\n')
+# for name in favLanguage.keys():
+#     print(name.title())
+
+# using an if loop to deal with specific keys
+friends = ['phil', 'sarah']
+for name in favLanguage.keys():
+    print(name.title())
+    if name in friends:
+        print(' Hi ' + name.title() + ', I see your favorite language is ' + favLanguage[name].title() + '!')
+
+if 'erin' not in favLanguage.keys():
+    print('Erin please take our pole about languages')
