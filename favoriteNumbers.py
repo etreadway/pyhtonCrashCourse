b3d14c38ccc17ccbb9c49876837efbb4428f8779
lowtierbeer <eric.s.treadway@gmail.com>
2020-09-21 12:36:08 -0500

    made the code more concise using the items method

--- favoriteNumbers.py
+++ favoriteNumbers.py
@@ -9,10 +9,14 @@ favoriteNumbers = {'hannah': 13,
                    'dalian': 4,
                    'raven': 89,}
 
-# building a loop to print all keys
-for name in favoriteNumbers:
-    print('\n'
-          + name.title()
-          + '\'s favorite number is '
-          + str(favoriteNumbers[name])
-          + '.')
+# # building a loop to print all keys
+# for name in favoriteNumbers:
+#     print('\n'
+#           + name.title()
+#           + '\'s favorite number is '
+#           + str(favoriteNumbers[name])
+#           + '.')
+
+# building the same loop but with the items method
+for name, number in favoriteNumbers.items():
+    print(name.title() + '\'s favorite number is ' + str(number) + '.\n')
