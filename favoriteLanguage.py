fc112ed818581b786b81d75314b6992a79dceb36
lowtierbeer <eric.s.treadway@gmail.com>
2020-09-22 17:56:44 -0500

    Updated for working with .keys(), set(), and .values()

--- favoriteLanguage.py
+++ favoriteLanguage.py
@@ -13,6 +13,23 @@ favLanguage = {
 #       '.')
 
 # printing using a for loop
+friends = ['phil', 'sarah']
+
 for name, language in favLanguage.items():
-    print(name.title() + '\'s favorite language is '
-          + language.title() + '.')
+    if name not in friends:
+        print(name.title() + ': ' + language)
+    else:
+        print('Hi ' + name.title() + ', I see your favorite language is ' +
+              favLanguage[name].title() + '!')
+
+if 'erin' not in favLanguage.keys():
+    print('Erin, please take the programming language poll!')
+
+# Using the sorted function
+for name in sorted(favLanguage.keys()):
+    print(name.title() + ', thank you for taking the poll!')
+
+# Looping through values
+print('\nThe following languages have been mentioned:')
+for language in set(favLanguage.values()):
+    print(language)
