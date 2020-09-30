aadf9f02fc785055e13a30005bafc1a2c221b7ff
lowtierbeer <eric.s.treadway@gmail.com>
2020-09-30 18:25:27 -0500

    adding a flag to a while loop

--- parrot.py
+++ parrot.py
@@ -1,2 +1,11 @@
-message = input('Tell me something, and I will repeat it back to you: ')
-print(message)
+prompt = 'Tell me something, and I will repeat it back to you: '
+
+active = True
+while active:
+    message = input(prompt)
+
+    if message.lower() == 'quit':
+        active = False
+    else:
+        print(message)
+        print('\nEnter "quit" to end the program')
