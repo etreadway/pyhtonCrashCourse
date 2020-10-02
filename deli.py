0a24ee6a7fe22fb041f040e97376028a66dbb1f2
lowtierbeer <eric.s.treadway@gmail.com>
2020-10-02 11:45:50 -0500

    moving elements from one list to another

--- deli.py
+++ deli.py
@@ -1,3 +1,4 @@
+# list of sandwiches that need to be made
 sandwichOrders = ['pb&j',
                   'turkey + cheese',
                   'chicken salad',
@@ -6,14 +7,15 @@ sandwichOrders = ['pb&j',
                   ]
 
 finishedSandwiches = []
-
+# moving each sandwich to the finished list when they are made
 while sandwichOrders:
     currentSandwich = sandwichOrders.pop()
     print('making your ' + currentSandwich + ' sandwich.')
 
     finishedSandwiches.append(currentSandwich)
-
+# Telling the costomer their order is ready
 print('\nYour order is finished')
+# Building a proper sentence structure using the list
 orderSentence = 'Come get your '
 for sandwich in finishedSandwiches[0:-1]:
     orderSentence += sandwich + ', '
