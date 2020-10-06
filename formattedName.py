bedb538e39d959517a7bd9034c568a8e4b39e4b1
lowtierbeer <eric.s.treadway@gmail.com>
2020-10-06 10:35:28 -0500

    Making a function arguments optional

--- formattedName.py
+++ formattedName.py
@@ -1,7 +1,14 @@
-def formattedName(firstName, lastName):
+def formattedName(firstName, lastName, middleName=''):
     '''Return a full name, neatly formatted.'''
-    fullName = firstName + ' ' + lastName
+    if middleName:
+        fullName = firstName + ' '\
+                   + middleName + ' '\
+                   + lastName
+    else:
+        fullName = firstName + ' ' + lastName
     return fullName.title()
 
 musician = formattedName('jimi', 'hendrix')
+print(musician)
+musician = formattedName('john', 'hooker', 'lee')
 print(musician)
