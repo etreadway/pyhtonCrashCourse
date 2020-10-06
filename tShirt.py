77589dce06c529546393907019fbd52381147c27
lowtierbeer <eric.s.treadway@gmail.com>
2020-10-06 07:28:20 -0500

    Messing with the basics of functions

---
+++ tShirt.py
@@ -0,0 +1,6 @@
+def makeShirt(msg='I love python', size='Large'):
+    print('The shirt will be ' + size + ' and say "' + msg + '" on the front.')
+
+makeShirt()
+makeShirt(size='Medium')
+makeShirt(msg='this is a shirt i guess')
