f64f6df9ae2c89fc1850823bccab2e22b937d44f
lowtierbeer <eric.s.treadway@gmail.com>
2020-10-07 07:35:34 -0500

    Playing with functions

---
+++ cityNames.py
@@ -0,0 +1,7 @@
+def cityCountry(city, country):
+    '''Formats a city and the country that it's in.'''
+    print(city.title() + ', ' + country.title())
+
+cityCountry('santiago', 'chile')
+cityCountry('paris', 'france')
+cityCountry('london', 'england')
