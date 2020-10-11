ca1b78fb80ebbfc7ef6fc494275040b7038bbed0
lowtierbeer <eric.s.treadway@gmail.com>
2020-10-11 10:22:21 -0500

    Using arbitrary kwyword arguments

---
+++ userProfile.py
@@ -0,0 +1,14 @@
+def buildProfile(first, last, **userInfo):
+    '''Build a dictionary containing everything we know about a user.'''
+    profile = {}
+    profile['firstName'] = first
+    profile['lastName'] = last
+    for key, value in userInfo.items():
+        profile[key] = value
+    return profile
+
+userProfile = buildProfile('albert', 'einstein', 
+                            location='princeton',
+                            feild='physics')
+
+print(userProfile)
