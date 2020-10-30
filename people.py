ca73f4e9431223399501d875d25c5ce91ac8c30b
LowTierBeer <eric.s.treadway@gmail.com>
2020-10-30 13:59:32 -0500

    cleaning up white space

--- people.py
+++ people.py
@@ -1,23 +1,23 @@
 
 people = {'wife':{
-              'first': 'boo',
-              'last': 'berry',
-              'age': 30,
-              'city': 'h-town',
-              },
-          'medium dog':{
-              'first': 'bug',
-              'last': 'friend',
-              'age': 5,
-              'city': 'h-town',
-              },
-          'small dog':{
-              'first': 'tiny',
-              'last': 'pup',
-              'age': 2,
-              'city': 'h-town',
-              },
-          }
+            'first': 'boo',
+            'last': 'berry',
+            'age': 30,
+            'city': 'h-town',
+            },
+        'medium dog':{
+            'first': 'bug',
+            'last': 'friend',
+            'age': 5,
+            'city': 'h-town',
+            },
+        'small dog':{
+            'first': 'tiny',
+            'last': 'pup',
+            'age': 2,
+            'city': 'h-town',
+            },
+        }
 
 for person, info in people.items():
     print('\n' + '-' * 20)
