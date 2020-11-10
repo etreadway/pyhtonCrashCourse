c7aef34f66657be5a1820159109e27fd2495c722
lowtierbeer <eric.s.treadway@gmail.com>
2020-11-10 07:56:43 -0600

    Counting all the words in a piece of text and handling UnicodeDecodeError

---
+++ alice.py
@@ -0,0 +1,18 @@
+# Use the codecs module so that we don't get a UnicodeDecodeError
+import codecs
+
+fileName = 'alice.txt'
+
+try:
+    with codecs.open(fileName, 'r', encoding='utf-8',
+                     errors='ignore') as fo:
+        content = fo.read()
+except FileNotFoundError:
+    msg = 'Sorry, the file ' + fileName + ' does not exist.'
+    print(msg)
+else:
+    # Count the approximate number of words in the file
+    words = content.split()
+    numWords = len(words)
+    print('The file ' + fileName + ' has about ' +
+          str(numWords) + ' words.')
