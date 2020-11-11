f64d270041f716028e168dd38c1ce3ed210b20c5
lowtierbeer <eric.s.treadway@gmail.com>
2020-11-11 10:45:44 -0600

    Refactored as a function

--- rememberMe.py
+++ rememberMe.py
@@ -1,16 +1,19 @@
 import json
 
-# Load the username, if it has been stored previously.
-# Otherwise, prompt for the username and store it.
-filename = 'username.json'
-try:
-    with open(filename) as fo:
-        username = json.load(fo)
-except FileNotFoundError:
-    username = input('What is your name? ')
-    with open(filename, 'w') as fo:
-        json.dump(username, fo)
-        print('We\'ll remember you when you come back, ' +
-              username + '!')
-else:
-    print('Welcome back, ' + username + '!')
+def greetUser():
+    '''
+    Load the username, if it has been stored previously.
+    Otherwise, prompt for the username and store it.
+    '''
+    filename = 'username.json'
+    try:
+        with open(filename) as fo:
+            username = json.load(fo)
+    except FileNotFoundError:
+        username = input('What is your name? ')
+        with open(filename, 'w') as fo:
+            json.dump(username, fo)
+            print('We\'ll remember you when you come back, ' +
+                  username + '!')
+    else:
+        print('Welcome back, ' + username + '!')
